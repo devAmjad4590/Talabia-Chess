eff5647e3b692223035efd146e6a2d192f3a99a4
MaherMMU <1221302826@student.mmu.edu.my>
2023-12-09 16:15:41 +0800

    Documentation

--- Board.java
+++ Board.java
@@ -1,14 +1,50 @@
-public class Board{
-    private static Board instance;
-    private static int length = 6;
-    private static int width = 7;
+/**
+ * This was class was written by Maher M.N. Balchi.
+ * The Board class represents the game board for a specific game.
+ * It follows the Singleton pattern to ensure only one instance of the board exists.
+ */
+public class Board {
+    private static Board instance;  // The single instance of the Board
+    private static int length = 6;   // The length of the board
+    private static int width = 7;    // The width of the board
+    private static Tile[][] tiles;   // 2D array to store the tiles on the board
 
+    /**
+     * Private constructor to prevent external instantiation.
+     * Initializes the board with the specified length and width.
+     *
+     * @param length The length of the board.
+     * @param width  The width of the board.
+     */
     private Board(int length, int width) {
-        //implemtation
-    };
+        createBoard(length, width);
+    }
 
+    /**
+     * Creates the board with the specified dimensions and initializes all tiles.
+     *
+     * @param length The length of the board.
+     * @param width  The width of the board.
+     */
+    private static void createBoard(int length, int width) {
+        tiles = new Tile[length][width];  // Initialize the tiles array
+
+        // Populate the board with Tile objects
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                tiles[i][j] = new Tile();
+            }
+        }
+    }
+
+    /**
+     * Gets the single instance of the Board class.
+     * If the instance does not exist, it is created.
+     *
+     * @return The Board instance.
+     */
     public static Board getInstance() {
-        if (instance == null){
+        if (instance == null) {
             instance = new Board(length, width);
         }
         return instance;
