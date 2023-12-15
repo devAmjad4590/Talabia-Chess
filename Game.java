220eecc90abe9c8ca8de0e17b101813c83c919d5
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2023-12-15 19:52:37 +0800

    modified game class

--- Game.java
+++ Game.java
@@ -9,47 +9,63 @@ public class Game {
     private int turn = 0; // The current turn number
     private Board board; // The game board
 
-
     /**
      * Initializes the game by creating player instances and game board.
      */
-    private void init(){
+    private void init() {
         board = Board.getInstance();
-        yellow = new Player();
-        blue = new Player();
+        yellow = new Player(true);
+        blue = new Player(false);
     }
 
     /**
      * Moves a player's piece from the current tile to the new tile.
      *
+     * @param player      The player to move
      * @param currentTile The current tile of the player's piece.
      * @param newTile     The new tile where the player's piece will be moved.
      */
-    private void move(Tile currenTile, Tile newTile){
-        // implementation
-        
+    public boolean isMoveValid(Player player, Tile currentTile, Tile newTile) {
+        Piece playerPiece = currentTile.getPiece(); // store the source piece in the playerPiece variable
+        if (player == getPlayerTurn() && playerPiece.canMove(currentTile, newTile)
+                && player.isYellow() == playerPiece.isYellow()) { // checks if it's the player's turn & legal move and if the piece belong to them
+            return true;
+        }
+        return false;
     }
 
+    public void playerMove(Player player, Tile currentTile, Tile newTile) {
+        if (isMoveValid(player, currentTile, newTile)) {
+            newTile.setPiece(currentTile.getPiece());
+            currentTile.setPiece(null);
+        } else {
+            System.out.println("Invalid move");
+        }
+
+    }
+
+    
 
     /**
      * Starts the game by initializing the game state and starting the first turn.
      */
-    public void startGame(){
+    public void startGame() {
         init();
+        
 
-        //implementation
+        // implementation
     };
 
     /**
      * Handles the logic when the game is over.
      */
-    public void gameOver(){
+    public void gameOver() {
         // implementation
     };
 
-
     /**
-     * Advances the game to the next turn, switching between yellow and blue players.
+     * Advances the game to the next turn, switching between yellow and blue
+     * players.
      */
     public void nextTurn() {
         // Switch turns between yellow and blue players
@@ -73,19 +89,19 @@ public class Game {
      *
      * @return The current Player turn.
      */
-    public Player getPlayerTurn(){
-        if(yellow.getTurn()){
+    public Player getPlayerTurn() {
+        if (yellow.getTurn()) {
             return yellow;
         }
         return blue;
     }
 
-     /**
+    /**
      * Sets the blue player for the game.
      *
      * @param player The blue player to set.
      */
-    public void setBluePlayer(Player player){
+    public void setBluePlayer(Player player) {
         this.blue = player;
     }
 
@@ -94,7 +110,7 @@ public class Game {
      *
      * @param player The yellow player to set.
      */
-    public void setYellowPlayer(Player player){
+    public void setYellowPlayer(Player player) {
         this.yellow = player;
     }
 
@@ -103,7 +119,7 @@ public class Game {
      *
      * @return The blue player.
      */
-    public Player getBluePlayer(){
+    public Player getBluePlayer() {
         return blue;
     }
 
@@ -112,7 +128,7 @@ public class Game {
      *
      * @return The yellow player.
      */
-    public Player getYellowPlayer(){
+    public Player getYellowPlayer() {
         return yellow;
     }
 
--- Player.java
+++ Player.java
@@ -1,6 +1,11 @@
 
 public class Player {
     private boolean isTurn = false;
+    private boolean yellow;
+
+    public Player(boolean yellow){
+        this.yellow = yellow;
+    }
 
     // by amgad elrashid gurashi eltayeb (ill document the comment later)
     public boolean getTurn() {
@@ -11,5 +16,11 @@ public class Player {
     public void setTurn(boolean isTurn) {
         this.isTurn = isTurn;
     }
+
+    public boolean isYellow() {
+        return yellow;
+    }
+
+    
 
 }
