4e209630b8133559bab64f7e14b4cde775bd99e7
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2024-01-20 20:24:03 +0800

    fixed an error

--- Talabia.java
+++ Talabia.java
@@ -6,8 +6,6 @@ public class Talabia {
     public static void main(String[] args) {
         Game model = new Game(); // model
         GameView view = new GameView(); // view 
-        SaveManager saveManager = new SaveManager(model); // save manager
-        saveManager.loadGame();
         Controller controller = new Controller(view, model); // controller
      
         }
